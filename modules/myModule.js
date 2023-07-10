a04b025ee4eabf1cc4d4ff6652d6541589366cf5
Juan <juanfernatubello@gmail.com>
2023-07-10 15:26:43 -0300

    changed my module

--- modules/myModule.js
+++ modules/myModule.js
@@ -1,11 +1,8 @@
 
-function myModule(){
-console.log("I'm a Module")
-<<<<<<< HEAD
-console.log("Added a change on my module!")
-console.log("Added a new change!")
-=======
->>>>>>> parent of 6fac34b (Added new message on my module)
+function myModule() {
+    console.log("I'm a Module")
+    console.log("Added a change on my module!")
+    console.log("Added a new change!")
 }
 
 module.exports = {
