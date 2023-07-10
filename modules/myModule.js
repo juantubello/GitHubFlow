1daae7d51039d758b4b13ecb69e17ab42fd9ee4e
Juan <juanfernatubello@gmail.com>
2023-07-10 15:14:28 -0300

    Se revirtieron cambios

--- modules/myModule.js
+++ modules/myModule.js
@@ -1,8 +1,11 @@
 
 function myModule(){
 console.log("I'm a Module")
+<<<<<<< HEAD
 console.log("Added a change on my module!")
 console.log("Added a new change!")
+=======
+>>>>>>> parent of 6fac34b (Added new message on my module)
 }
 
 module.exports = {
